dbb44b7219f2a2be73fa1f678aee031409474d1b
hamaguchi mao <kupokupo44@gmail.com>
2025-08-29 16:32:24 +0900

    1-15ファイル作成まで

--- src/routes/web.php
+++ src/routes/web.php
@@ -15,3 +15,5 @@ use App\Http\Controllers\AuthorController;
 */
 
 Route::get('/', [AuthorController::class, 'index']);
+Route::get('/add', [AuthorController::class, 'add']);
+Route::post('/add', [AuthorController::class, 'create']);
